61003959adba961045ab9eea8a2f2bec6d5b8c2a
Nadav Dav <nadavik@gmail.com>
2015-07-05 16:27:28 +0300

    last changes reverted and fixed again :D

--- stages/comments.js
+++ stages/comments.js
@@ -3,8 +3,11 @@ var Promise = require('bluebird');
 module.exports = function (input){
   var out = input;
   return new Promise(function (resolve) {
-    out = out.replace(/\s(\/\/[^\n]*)\n/g,"<span class='comment'>$1</span>");
-    out = out.replace(/^(\/\/[^\n]*)\n/g,"<span class='comment'>$1</span>");
+    out = out.replace(/(http\:\/\/)/ig,"_HTTPTOKEN_");
+    out = out.replace(/(https\:\/\/)/ig,"_HTTPSTOKEN_");
+    out = out.replace(/(\/\/[^\n]*)\n/g,"<span class='comment'>$1</span>");
+    out = out.replace(/(_HTTPTOKEN_)/g,"http://");
+    out = out.replace(/(_HTTPSTOKEN_)/g,"https://");
     resolve(out);
   });
 };
